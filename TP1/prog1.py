4245f64c25281998a67f8fc28c6e89bdc5ec8a57
bonenjul <jbonenfant27@gmail.com>
2024-09-11 09:42:51 +0200

    Ajout TP2 V2

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,10 +1,25 @@
 # Affiche "Hello, World!" au lancement
 print("Hello, World!")
 
-# Demander à l'utilisateur de saisir un nombre
-nombre = float(input("Entrez un nombre : "))
+import fonctions as f
 
-# Calculer et afficher le carré du nombre
-carre = nombre ** 2
-print(f"Le carré de {nombre} est {carre}")
+# Boucle infinie
+while True:
+    try:
+        # Demander à l'utilisateur de saisir deux nombres
+        a = float(input("Entrez le premier nombre (ou CTRL-C pour quitter) : "))
+        b = float(input("Entrez le second nombre : "))
 
+        # Convertir les nombres en entiers pour tester la fonction
+        a_int = int(a)
+        b_int = int(b)
+
+        # Calculer et afficher a élevé à la puissance b
+        resultat = f.puissance(a_int, b_int)
+        print(f"{a_int} élevé à la puissance {b_int} est {resultat}")
+
+    except TypeError as e:
+        print(f"Erreur : {e}")
+    except KeyboardInterrupt:
+        print("\nProgramme interrompu. Au revoir !")
+        break
