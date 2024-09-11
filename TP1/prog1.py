5207041cc38155e5915772e573e440b9bb1215b6
bonenjul <jbonenfant27@gmail.com>
2024-09-11 09:18:32 +0200

    Ajout de la fonctionnalité pour calculer le carré d'un nombre

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,10 @@
+# Affiche "Hello, World!" au lancement
 print("Hello, World!")
+
+# Demander à l'utilisateur de saisir un nombre
+nombre = float(input("Entrez un nombre : "))
+
+# Calculer et afficher le carré du nombre
+carre = nombre ** 2
+print(f"Le carré de {nombre} est {carre}")
+
